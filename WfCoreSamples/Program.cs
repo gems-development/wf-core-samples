46eb1d2b4c530d03cdcad148ee8328750c1040a5
Дмитрий Долгушин <ddolgushin@yandex.ru>
2020-05-14 23:39:05 +0600

    + Initial commit.

---
+++ WfCoreSamples/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using WorkflowCore.Interface;
+using WfCoreSamples.Workflows;
+
+namespace WfCoreSamples
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var serviceProvider = ConfigureServices();
+			var host = serviceProvider.GetService<IWorkflowHost>();
+
+			// Регистрируем бизнес-процесс с классом данных.
+			host.RegisterWorkflow<FizzBuzzWorkflow, FizzBuzzWfData>();
+			// Отмечаем процесс для запуска.
+			host.StartWorkflow(FizzBuzzWorkflow.WorkflowDefinitionId);
+			// Стартуем движок.
+			host.Start();
+			Console.ReadKey();
+			host.Stop();
+		}
+
+		private static IServiceProvider ConfigureServices()
+		{
+			var services = new ServiceCollection();
+
+			services.AddLogging();
+			services.AddWorkflow();
+
+			return services.BuildServiceProvider();
+		}
+	}
+}
---
+++ WfCoreSamples/Workflows/FizzBuzzWorkflow.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace WfCoreSamples.Workflows
+{
+	public class FizzBuzzWfData
+	{
+		public int Counter { get; set; } = 1;
+		public StringBuilder Output { get; set; } = new StringBuilder();
+	}
+
+	public class FizzBuzzWorkflow : IWorkflow<FizzBuzzWfData>
+	{
+		public const string WorkflowDefinitionId = "FizzBuzz";
+
+		public string Id => WorkflowDefinitionId;
+		public int Version => 1;
+
+		public void Build(IWorkflowBuilder<FizzBuzzWfData> builder)
+		{
+			builder
+				.StartWith(context => ExecutionResult.Next())
+				.While(data => data.Counter <= 100)
+					.Do(a => a
+						.StartWith(context => ExecutionResult.Next())
+							.Output((step, data) => data.Output.Append(data.Counter))
+						.If(data => data.Counter % 3 == 0 || data.Counter % 5 == 0)
+							.Do(b => b
+								.StartWith(context => ExecutionResult.Next())
+									.Output((step, data) => data.Output.Clear())
+								.If(data => data.Counter % 3 == 0)
+									.Do(c => c
+										.StartWith(context => ExecutionResult.Next())
+											.Output((step, data) => data.Output.Append("Fizz")))
+								.If(data => data.Counter % 5 == 0)
+									.Do(c => c
+										.StartWith(context => ExecutionResult.Next())
+											.Output((step, data) => data.Output.Append("Buzz"))))
+						.Then(context => ExecutionResult.Next())
+							.Output((step, data) =>
+							{
+								Console.WriteLine(data.Output.ToString());
+								data.Output.Clear();
+
+								data.Counter++;
+							}));
+		}
+	}
+}
